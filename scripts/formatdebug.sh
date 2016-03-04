1b9af0f60c0078519f02abc6a2980f87967d27d7
msegade <miguelrsegade@gmail.com>
2016-03-04 13:44:07 +0100

    Added script to format debug

--- scripts/formatdebug.sh
+++ scripts/formatdebug.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-awk '$1 in a {print a[$1], $0; next}; {a[++l] = $0}' < "$1"
+#awk '$1 in a {print a[$1], $0; next}; {a[++l] = $0}' < "$1"
+gawk '
+  $1 in a { a[$1] = a[$1] " " $0; next }
+  { a[++l] = $0 }
+  END { for (i = 1; i <= length(a); i++) print a[i] }
+' < "$1"
